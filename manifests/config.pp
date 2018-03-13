8909edc994a493e43d07fbc83d592969f4f220d0
Cyril Pinkham <cyril.pinkham@bigcommerce.com>
2018-03-12 17:32:13 -0700

    fixes

--- manifests/config.pp
+++ manifests/config.pp
@@ -185,7 +185,7 @@ class hubot::config {
       owner   => 'hubot',
       group   => 'hubot',
       mode    => '0444',
-      source => "puppet:///modules/${module_name}/package.json",
+      content => template('hubot/package.json.erb'),
       notify  => Class['hubot::service'],
       require => Exec['Hubot init'],
     }
